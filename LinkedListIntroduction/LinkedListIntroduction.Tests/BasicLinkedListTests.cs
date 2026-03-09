995c18eba9a3fe305f27234a989e5aa9cab21538
Kshitij Dubey <dubeyk@stpaulsschool.org.uk>
2026-03-09 22:53:57 +0000

    Linked list tasks

--- LinkedListIntroduction/LinkedListIntroduction.Tests/BasicLinkedListTests.cs
+++ LinkedListIntroduction/LinkedListIntroduction.Tests/BasicLinkedListTests.cs
@@ -1,4 +1,5 @@
-﻿using LinkedListIntroduction.Lib; 
+﻿using LinkedListIntroduction.Lib;
+using Mono.Cecil.Cil;
 
 namespace LinkedListIntroduction.Tests;
 
@@ -37,6 +38,79 @@ public sealed class BasicLinkedListTests
         var ill = new IntegerLinkedList(5);
         ill.Append(7);
         ill.Append(9);
+        Assert.AreEqual("{5, 7, 9}", ill.ToString());
+    }
+
+    [TestMethod]
+    public void TestPrepend()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Prepend(7);
+        ill.Prepend(9);
+        Assert.AreEqual("{9, 7, 5}", ill.ToString());
+    }
+
+    [TestMethod]
+    public void TestDelete()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+
+        ill.Delete(7);
+
+        Assert.AreEqual("{5, 9}", ill.ToString());
+    }
+
+    [TestMethod]
+    public void TestInsert()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+
+        ill.Insert(3, 1);
+        Assert.AreEqual("{5, 3, 7, 9}", ill.ToString());
+    }
+
+    [TestMethod]
+    public void TestJoin()
+    {
+        var ill1 = new IntegerLinkedList(5);
+        ill1.Append(7);
+        ill1.Append(9);
+
+        var ill2 = new IntegerLinkedList(6);
+        ill2.Append(4);
+        ill2.Append(2);
+
+        ill1.Join(ill2);
+
+        Assert.AreEqual("{5, 7, 9, 6, 4, 2}", ill1.ToString());
+    }
+
+    [TestMethod]
+    public void TestContains()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+
+        Assert.IsTrue(ill.Contains(7));
+    }
+
+    [TestMethod]
+    public void TestRemoveDuplicates()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(7);
+        ill.Append(9);
+        ill.Append(9);
+        ill.Append(9);
+
+        ill.RemoveDuplicates();
+
         Assert.AreEqual("{5, 7, 9}", ill.ToString());
     }
 
